79d7e2ce18304b9be7cf4da9f644add8dbc57885
Bati <no-reply@batijs.dev>
2025-01-27 17:31:30 +0700

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { pages } from "vike-cloudflare";
+import { telefunc } from "telefunc/vite";
+import react from "@vitejs/plugin-react";
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [
+    vike({}),
+    devServer({
+      entry: "hono-entry.ts",
+
+      exclude: [
+        /^\/@.+$/,
+        /.*\.(ts|tsx|vue)($|\?)/,
+        /.*\.(s?css|less)($|\?)/,
+        /^\/favicon\.ico$/,
+        /.*\.(svg|png)($|\?)/,
+        /^\/(public|assets|static)\/.+/,
+        /^\/node_modules\/.*/,
+      ],
+
+      injectClientScript: false,
+    }),
+    react({}),
+    telefunc(),
+    pages({
+      server: {
+        kind: "hono",
+        entry: "hono-entry.ts",
+      },
+    }),
+  ],
+
+  build: {
+    target: "es2022",
+  },
+});
